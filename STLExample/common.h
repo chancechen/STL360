91d22a77c4bc513b6c5874129b9f8010ef66a52a
chenyong <cy_chance@hotmail.com>
2016-06-14 16:59:17 +0800

    add std study example!

---
+++ STLExample/common.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
---
+++ STLExample/main.cpp
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv) {
+	cout << "welcome to STL360 trainning"<< endl;
+	return 0;
+}
