6f30a0533ca5b7e18381a312b98a6239f9ca4d07
chenyong <cy_chance@hotmail.com>
2016-06-14 20:56:03 +0800

    add base object for running example

--- STLExample/include/copyright.h
+++ STLExample/include/copyright.h
@@ -6,6 +6,16 @@
 // email      : cy_chance@hotmail.com
 // createtime : 2016/6/14
 #pragma once
+// head descripte ,example
+// Copyright (c) 2016 chen
+// All rights reserved.
+// desc : the copyright information for stl360
+// version    :  0.0.1
+// author     :  chen
+// email      : cy_chance@hotmail.com
+// createtime : 2016/6/14
+
+// program inforamtion for console 
 #define cur_name "STLExample"
 #define cur_version "0.0.1"
 #define cur_author "chenyong"
